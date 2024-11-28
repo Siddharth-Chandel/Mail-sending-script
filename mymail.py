8b0de3e8b0bca9b56daa322ab705c9aba40f411b
Siddharth Chandel <90092814+Siddharth-Chandel@users.noreply.github.com>
2024-11-28 10:18:36 +0530

    Update mymail.py

--- mymail.py
+++ mymail.py
@@ -5,73 +5,68 @@ def send_email(receiver_email: str, sub: str = 'Program Info', text: str = '', s
     from email.mime.multipart import MIMEMultipart
     import smtplib
     import os
-    # initialize connection to our
-    # email server, we will use gmail here
-    smtp = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp.ehlo()
-    smtp.starttls()
 
-    # Login with your email and password
-    if sender is None or password is None:
-        sender = os.environ['Gmail ID']
-        password = os.environ['Gmail Passkey']
-    smtp.login(sender, password)
+    try:
+        # Initialize connection to Gmail's SMTP server
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.ehlo()
+        smtp.starttls()
 
-    def message(subject: str = "Python Notification",
-                text: str = "", img=None,
-                attachment=None):
-        '''Send our email message 'msg' to our boss'''
-        # build message contents
-        msg = MIMEMultipart()
-        msg['Subject'] = subject
-        msg.attach(MIMEText(text))
+        # Login with provided credentials or environment variables
+        if sender is None or password is None:
+            sender = os.environ.get('Gmail_ID')
+            password = os.environ.get('Gmail_Passkey')
+            if sender is None or password is None:
+                raise ValueError("Email credentials are not provided.")
 
-        if img is not None:
-            # Check whether we have the lists of images or not!
-            if type(img) is not list:
-                img = [img]
+        smtp.login(sender, password)
 
-            for one_img in img:
-                # read the image binary data
-                img_data = open(
-                    f"{os.getcwd()}/images/{one_img}", 'rb').read()
-                # Attach the image data to MIMEMultipart
-                # using MIMEImage, we add the given filename use os.basename
-                msg.attach(MIMEImage(img_data,
-                                     name=os.path.basename(one_img)))
+        # Build the email message
+        def message(subject: str, text: str, img=None, attachment=None):
+            msg = MIMEMultipart()
+            msg['From'] = sender
+            msg['To'] = receiver_email
+            msg['Subject'] = subject
+            msg.attach(MIMEText(text, 'plain'))
 
-        # Check whether we have the lists of attachments or not!
-        if attachment is not None:
-            # Check whether we have the lists of attachments or not!
-            if type(attachment) is not list:
-                attachment = [attachment]
+            if img:
+                if not isinstance(img, list):
+                    img = [img]
+                for one_img in img:
+                    with open(one_img, 'rb') as f:
+                        img_data = f.read()
+                        msg.attach(MIMEImage(img_data, name=os.path.basename(one_img)))
 
-            for one_attachment in attachment:
-                with open(one_attachment, 'rb') as f:
-                    # Read in the attachment
-                    # using MIMEApplication
-                    file = MIMEApplication(
-                        f.read(),
-                        name=os.path.basename(one_attachment)
-                    )
-                file['Content-Disposition'] = f'attachment;\filename="{os.path.basename(one_attachment)}"'
+            if attachment:
+                if not isinstance(attachment, list):
+                    attachment = [attachment]
+                for one_attachment in attachment:
+                    with open(one_attachment, 'rb') as f:
+                        file = MIMEApplication(f.read(), name=os.path.basename(one_attachment))
+                        file['Content-Disposition'] = f'attachment; filename="{os.path.basename(one_attachment)}"'
+                        msg.attach(file)
 
-                # At last, Add the attachment to our message object
-                msg.attach(file)
-        return msg
+            return msg
 
-    msg = message(subject=sub, text=text,
-                  img=img, attachment=attach)
+        msg = message(subject=sub, text=text, img=img, attachment=attach)
 
-    # Make a list of emails, where you wanna send mail
-    to = receiver_email
+        # Send the email
+        smtp.sendmail(from_addr=sender, to_addrs=receiver_email, msg=msg.as_string())
+        print("Email sent successfully!")
 
-    # Provide some data to the sendmail function!
-    smtp.sendmail(from_addr=sub,
-                  to_addrs=to, msg=msg.as_string())
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        smtp.quit()
 
-    # Finally, don't forget to close the connection
-    smtp.quit()
 
 if __name__ == '__main__':
-    send_email('test@gmail.com')
+    send_email(
+        receiver_email='test@gmail.com',
+        sub='Test Email',
+        text='This is a test email.',
+        sender='youremail@gmail.com',
+        password='yourpassword',
+        img=None,
+        attach=None
+    )
